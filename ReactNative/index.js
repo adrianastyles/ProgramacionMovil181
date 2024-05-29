eb983c68c7f5f4a5e320374a1afec40c8cae9228
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2024-05-28 20:26:31 -0600

    ACTIVIDAD 2

---
+++ ReactNative/index.js
@@ -0,0 +1,21 @@
+console.log('Hola consola');
+
+//FUNCIONES
+
+
+//TRADICIONAL
+function suma(a, b){
+    return a + b;
+}
+
+//FAT ARROW
+const resta = (a, b) => {return a - b};
+
+//SIMPLIFICADO FAT ARROW
+const resta2 = (a, b) => a - b;
+
+//SUMA
+console.log(suma(1, 10));
+
+//RESTA
+console.log(resta(100, 10));
